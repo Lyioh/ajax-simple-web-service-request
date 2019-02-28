35514b23ce7d7ed0bfca48796367308073b97a1a
Lyioh <bidegaray.lionel@gmail.com>
2019-02-28 15:55:22 +0100

    test login

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-/* ////////////////////////// Fetch --- Promise /////////////////////////////
-    fetch('https://thatsthespir.it/api')
-    .then(res => res.json())//response type
-    .then(data => console.log(data)); //log the data; */
+/* 
+bonjour
 
-let objReceived = new Object;
-
-//Create the XHR Object
 let xhr = new XMLHttpRequest;
-//Call the open function, GET-type of request, url, true-asynchronous
+
 xhr.open('GET', 'https://thatsthespir.it/api', true)
-//call the onload 
+
 xhr.onload = function () {
-    //check if the status is 200(means everything is okay)
+
     if (this.status === 200) {
-        //return server response as an object with JSON.parse
+        let objReceived = new Object;
+
         console.log(JSON.parse(this.responseText));
         objReceived = JSON.parse(this.responseText)
         document.querySelector("#author").innerHTML = "Author: " + objReceived.author;
@@ -24,12 +20,42 @@ xhr.onload = function () {
         alert("FORBIDDEN!!!");
     }
 }
-//call send
-console.log(xhr.response)
-xhr.send();
-    //Common Types of HTTP Statuses
-    // 200: OK
-    // 404: ERROR
-    // 403: FORBIDDEN
 
-   
+xhr.send();
+ */
+////////////////////////////////////// The OLD WAY /////////////////////////////////////////////////////////
+/* function request(callback) {
+    let xhr = new XMLHttpRequest;
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            callback(xhr.responseText);
+        } else if (this.status === 403) {
+            alert("ERROR!!!");
+        } else if (this.status === 404) {
+            alert("FORBIDDEN!!!");
+        }
+    }
+
+    xhr.open('GET', 'https://thatsthespir.it/api', true)
+    xhr.send();
+}
+
+function theData(data) {
+    let objReceived = new Object;
+    objReceived = JSON.parse(data)
+    console.log(JSON.parse(data));
+    document.querySelector("#author").innerHTML = "Author: " + objReceived.author;
+    document.querySelector("#quote").innerHTML = objReceived.quote;
+}
+
+request(theData); */
+
+/////////////////////////////////////// Avec le fetch ////////////////////////////////////////////////////////
+
+fetch('https://thatsthespir.it/api')
+    .then(res => res.json())//response type
+    .then(data => {
+        document.querySelector("#author").innerHTML = "Author: " + data.author;
+        document.querySelector("#quote").innerHTML = data.quote;
+    });
